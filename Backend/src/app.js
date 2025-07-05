7f6774c07801ed94f51037ce9ffde3d1393daef0
tanujmm <mtanuj121@gmail.com>
2025-07-06 00:27:49 +0530

    appEdit commit

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,14 +1,42 @@
-const express = require("express");
-const aiRoutes = require("./routes/ai.routes.js");
-const app = express();
-const cors = require("cors");
+// const express = require("express");
+// const aiRoutes = require("./routes/ai.routes.js");
+// const app = express();
+// const cors = require("cors");
 
-app.use(cors());
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+// app.use(cors());
+// app.get("/", (req, res) => {
+//   res.send("hello world");
+// });
+
+// app.use(express.json());
+
+// app.use("/ai", aiRoutes);
+
+// module.exports = app;
+
+const express = require("express");
+const cors = require("cors");
+const aiRoutes = require("./routes/ai.routes.js");
+
+const app = express();
+
+const allowedOrigins = [
+  "https://ai-powered-code-reviewer-frontend-tle8.onrender.com",
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/ai", aiRoutes);
 
