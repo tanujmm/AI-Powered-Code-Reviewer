a72eb07d8c9cb2b2bcb1f1bb36b1f792fd931329
tanujmm <mtanuj121@gmail.com>
2025-07-05 23:15:05 +0530

    button6 commit

--- Backend/src/controllers/ai.controller.js
+++ Backend/src/controllers/ai.controller.js
@@ -1,12 +1,3 @@
-// const aiService = require("../services/ai.service.js");
-
-// module.exports.getResponse = async (req, res) => {
-//   const prompt = req.query.prompt;
-//   if (!prompt) {
-//     return res.status(400).json("Prompt is required");
-//   }
-//   const response = await aiService(prompt);
-// };
 const aiService = require("../services/ai.service.js");
 
 module.exports.getReview = async (req, res) => {
