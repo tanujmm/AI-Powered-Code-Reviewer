f6b24d22f828a9236666cf11efbcb273a5e3fc28
tanujmm <mtanuj121@gmail.com>
2025-07-06 00:59:04 +0530

    env2 commit

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 // app.listen(3000, () => {
 //   console.log("Server is running at PORT 3000");
 // });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server started on port", PORT);
 });
--- Backend/src/controllers/ai.controller.js
+++ Backend/src/controllers/ai.controller.js
@@ -1,18 +1,3 @@
-// const aiService = require("../services/ai.service.js");
-
-// module.exports.getReview = async (req, res) => {
-//   const { code } = req.body;
-//   if (!code) {
-//     return res.status(400).json("Prompt is required");
-//   }
-//   try {
-//     const response = await aiService(code);
-//     res.status(200).json({ response });
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
 const aiService = require("../services/ai.service.js");
 
 module.exports.getReview = async (req, res) => {
