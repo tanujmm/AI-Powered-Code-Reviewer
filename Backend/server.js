7642ac4d994bfaf40f736319e6e884586f9fc5d0
tanujmm <mtanuj121@gmail.com>
2025-07-05 16:44:34 +0530

    second commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,10 @@
 const app = require("./src/app.js");
 require("dotenv").config();
 
-app.listen(3000, () => {
-  console.log("Server is running at PORT 3000");
+// app.listen(3000, () => {
+//   console.log("Server is running at PORT 3000");
+// });
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server started on port", PORT);
 });
